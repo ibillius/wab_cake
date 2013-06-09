6d37aefd19d2e2f863ffaaac38d526ce9dd3a204
ibillius <wss.simpson@gmail.com>
2013-06-09 16:52:10 -0700

    UNLIMITED MEOWS!

--- public/app/webroot/js/main.js
+++ public/app/webroot/js/main.js
@@ -3,13 +3,15 @@
   var woozle_index = 0;
 
   $(document).keydown(function(e){
-      if(e.keyCode === woozle_keys[woozle_index++]){
+      if(e.keyCode === woozle_keys[woozle_index++] ){
          if(woozle_index === woozle_keys.length){
-            $(document).unbind('keydown', arguments.callee);
+            //Uncomment to restrict users to one call.
+            //$(document).unbind('keydown', arguments.callee);
             document.getElementById('meow').play();
             $('#woozle').animate({"bottom" : "0","left" : "0", 'display':'block' }, 300, function(){
               $(this).delay(2000).animate({"bottom" : "-571px", "left" : "-410px", 'display' : 'none'}, 300);
             });
+            woozle_index = 0;
          }
       }else{
           woozle_index = 0;
